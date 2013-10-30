e48bf0ce60cd446130fc0ffa310bf55ac99f3acb
Wouter Geraedts <git@woutergeraedts.nl>
2013-10-30 20:43:57 +0100

    scraper: added recursive category lookup

--- src/scraper.cpp
+++ src/scraper.cpp
@@ -1,6 +1,7 @@
 #include "scraper.hpp"
 
 #include <iostream>
+#include <stack>
 
 #include "parsers/category_listing_parser.hpp"
 
@@ -19,7 +20,23 @@ namespace supermarx
 		product mudcrab{"Mudcrab Sticks", 1337};
 		callback(mudcrab);
 
-		category_listing_parser cat_parser([&](const category_listing_parser::category_crumb_t& crumb) { std::cout << crumb << std::endl; });
-		cat_parser.parse(dl.fetch("http://www.ah.nl/appie/producten"));
+		std::stack<std::string> stack;
+		stack.emplace("http://www.ah.nl/appie/producten");
+
+		while(!stack.empty())
+		{
+			const std::string url = stack.top();
+			stack.pop();
+
+			category_listing_parser cat_parser(
+				[&](const category_listing_parser::category_crumb_t& crumb)
+				{
+					stack.emplace(url + "/" + crumb);
+				}
+			);
+
+			std::cout << url << std::endl;
+			cat_parser.parse(dl.fetch(url));
+		}
 	}
 }
